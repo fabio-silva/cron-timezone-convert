3934ab5365e56bb36a4e1792222d13598cc33c49
Fabio Silva <ffjs1993@gmail.com>
2024-06-15 17:19:05 +0100

    chore: include typings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from "vite-plugin-dts";
+
 
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
